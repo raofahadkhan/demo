caa05e2048717eee62f3b176f4b63279c3689a6a
Rao Fahad <raofahad046@gmail.com>
2024-01-26 00:31:23 +0500

    demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-  <div>{process.env.NEXT_PUBLIC_DEMO}</div>
-  );
+  return <div style={{ color: "white" }}>{process.env.NEXT_PUBLIC_DEMO}</div>;
 }
